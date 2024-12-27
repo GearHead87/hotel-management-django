09b5276b3d823dc9074e7e4b9fdfe949b29f83eb
GearHead87 <hosanulislam87@gmail.com>
2024-12-27 22:35:16 +0600

    fix(settings.py): stmp and sslcommerz fixed

--- djangomart/settings.py
+++ djangomart/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 import os
 from decouple import config
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -143,12 +142,13 @@ MESSAGE_TAGS = {
 }
 # SMTP configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_HOST = config('EMAIL_HOST')
+DEFAULT_FROM_EMAIL= config('EMAIL_HOST_USER')
+EMAIL_PORT = 587 # Default
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 
 SSLCOMMERZ_PAYMENT_URL = 'https://sandbox.sslcommerz.com/gwprocess/v4/api.php'  # Update with production URL for live environment
-SSLCOMMERZ_STORE_ID = 'djang64a00838ddbc2'
-SSLCOMMERZ_STORE_PASSWORD = 'RUf4qUafVnvcRNN'
+SSLCOMMERZ_STORE_ID = config('SSLCOMMERZ_STORE_ID')
+SSLCOMMERZ_STORE_PASSWORD = config('SSLCOMMERZ_STORE_PASSWORD')
