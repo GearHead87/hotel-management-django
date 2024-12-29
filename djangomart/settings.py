3235f5252c527f89601302aa77ce355d770e755e
GearHead87 <hosanulislam87@gmail.com>
2024-12-29 20:48:04 +0600

    update: database config from env

--- djangomart/settings.py
+++ djangomart/settings.py
@@ -111,11 +111,11 @@ WSGI_APPLICATION = 'djangomart.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "mysite",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "NAME": config('DATABASE_NAME'),
+        "USER": config('DATABASE_USER'),
+        "PASSWORD": config('DATABASE_PASSWORD'),
+        "HOST": config('DATABASE_HOST'),
+        "PORT": config('DATABASE_PORT'),
     }
 }
 
